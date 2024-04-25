21ca18f3ccdd10b9e36014c05ecd6d14b2531d66
casey <caseymwangi31@gmail.com>
2024-04-25 12:05:32 +0300

    understanding keys and lists

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Links from "./Links";
 
-function About({ bio, links }) {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {bio && bio.length > 1 ? <p>{bio}</p> : null}
+      {props.bio && props.bio !== "" && <p>{props.bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={links.github} linkedin={links.linkedin} />
+     {props.links && <Links github={props.links.github} linkedin={props.links.linkedin} />}
     </div>
   );
 }
